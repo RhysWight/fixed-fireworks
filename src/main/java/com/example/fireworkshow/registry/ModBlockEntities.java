3c36fcf9604c2bad8af26deb31442890af8f2058
RhysWight <rhysdwaynewight@gmail.com>
2025-07-05 21:14:42 -0600

    deletdfs

--- src/main/java/com/example/fireworkshow/registry/ModBlockEntities.java
+++ src/main/java/com/example/fireworkshow/registry/ModBlockEntities.java
@@ -18,25 +18,3 @@ public final class ModBlockEntities {
                     () -> BlockEntityType.Builder.of(FireworkLauncherBlockEntity::new,
                             ModBlocks.FIREWORK_LAUNCHER.get()).build(null));
 }
-
-package com.example.fireworkshow.registry;
-
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTab.Output;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import com.example.fireworkshow.FireworkShowMod;
-
-@Mod.EventBusSubscriber(modid = FireworkShowMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class ModCreativeTabContents {
-
-    @SubscribeEvent
-    public static void addToCreativeTabs(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTabKey() == CreativeModeTabs.REDSTONE_BLOCKS) {
-            event.accept(ModBlocks.FIREWORK_LAUNCHER.get().asItem());
-        }
-    }
-} 
