5c15724b3f6dace3d4025afd0d363996fe02b5a1
RhysWight <rhysdwaynewight@gmail.com>
2025-07-05 21:04:14 -0600

    adding stuff

--- src/main/java/com/example/fireworkshow/registry/ModBlockEntities.java
+++ src/main/java/com/example/fireworkshow/registry/ModBlockEntities.java
@@ -1,5 +1,7 @@
 package com.example.fireworkshow.registry;
 
+
+
 import com.example.fireworkshow.FireworkShowMod;
 import com.example.fireworkshow.content.FireworkLauncherBlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -16,3 +18,25 @@ public final class ModBlockEntities {
                     () -> BlockEntityType.Builder.of(FireworkLauncherBlockEntity::new,
                             ModBlocks.FIREWORK_LAUNCHER.get()).build(null));
 }
+
+package com.example.fireworkshow.registry;
+
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTab.Output;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import com.example.fireworkshow.FireworkShowMod;
+
+@Mod.EventBusSubscriber(modid = FireworkShowMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModCreativeTabContents {
+
+    @SubscribeEvent
+    public static void addToCreativeTabs(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.REDSTONE_BLOCKS) {
+            event.accept(ModBlocks.FIREWORK_LAUNCHER.get().asItem());
+        }
+    }
+} 
